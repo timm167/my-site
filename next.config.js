d39a27157514e2109978b7b83855d0aacc77d2bf
Tim Charteris <tim.charterii@gmail.com>
2025-02-18 16:51:10 +0000

    debug again

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   images: {
+    loader: 'default',
     domains: ["tim-charteris-portfolio.netlify.app", "localhost"],
   },
 }
